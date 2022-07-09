9279290a13a41d842ba29c409a5e7924560336af
8Eithel8 <eithel@list.ru>
2022-07-09 11:24:20 +0300

    feat: add  order save into local storage

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,36 @@
 const informers = document.querySelector('.informers');
-const informersElements = Array.from(informers.querySelectorAll('.informers__item'));
 const links = Array.from(informers.querySelectorAll('.link'));
 const images = Array.from(informers.querySelectorAll('img'));
 
+const getElements = () => Array.from(informers.querySelectorAll('.informers__item'));
+// const dataId = Array.from(informers.querySelectorAll('[data-id]'));
+
+
+//устанавливаем значения атрибута draggable
 links.forEach((item) => item.draggable = false);
-
 images.forEach((item) => item.draggable = false);
+getElements().forEach((item) => item.draggable = true);
 
-informersElements.forEach((item) => item.draggable = true);
 
+function getOrder () {
+    //находим и сохраняем в массив значения атрибута data-id всех элементов
+    const newArr = getElements().map((item) => item.getAttribute('data-id')).join('|');
+    //сохраняем порядок в LS
+    localStorage.setItem('dataOrder', newArr);
+}
+
+
+// устанавливаем слушатели
 informers.addEventListener(`dragstart`, (evt) => {
     evt.target.classList.add(`selected`);
 });
 
 informers.addEventListener(`dragend`, (evt) => {
     evt.target.classList.remove(`selected`);
+    getOrder();
 });
 
+//получаем координаты и возвращаем следующий
 const getNextElement = (cursorPosition, currentElement) => {
     const currentElementCoord = currentElement.getBoundingClientRect();
     const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
@@ -26,6 +40,7 @@ const getNextElement = (cursorPosition, currentElement) => {
         currentElement.nextElementSibling;
 };
 
+//устанавливаем слушатель на перетаскивание
 informers.addEventListener(`dragover`, (evt) => {
     evt.preventDefault();
 
@@ -50,3 +65,16 @@ informers.addEventListener(`dragover`, (evt) => {
 
     informers.insertBefore(activeElement, nextElement);
 });
+
+
+
+
+  // function getOrder () {
+  //           const order = localStorage.getItem(informersOrders);
+  //           return order ? order.split('|') : [];
+  // };
+
+ //  function  () {
+ //            var order = sortable.toArray();
+ //            localStorage.setItem(sortable.options.group.name, order.join('|'));
+ // }
